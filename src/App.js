829bfb6c70add2393d6815ab96a1149123abeac3
gfR616 <616gfr@gmail.com>
2024-01-10 05:36:11 +0500

    +

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react"
 import { ChakraProvider } from "@chakra-ui/react"
-import { HashRouter as Router, Route, Navigate, Routes } from "react-router-dom"
+import {
+  BrowserRouter as Router,
+  Route,
+  Navigate,
+  Routes
+} from "react-router-dom"
 import RegistrationPage from "./components/pages/registrationPage"
 import LoginPage from "./components/pages/loginPage"
 import ChatPage from "./components/pages/chatPage"
@@ -10,8 +15,8 @@ function App() {
     <ChakraProvider>
       <Router>
         <Routes>
-          <Route path='/register' element={<RegistrationPage />} />
           <Route exact path='/auth' element={<LoginPage />} />
+          <Route path='/register' element={<RegistrationPage />} />
           <Route exact path='/chat' element={<ChatPage />} />
           <Route path='/' element={<Navigate to='/auth' />} />
         </Routes>
--- src/components/pages/loginPage.jsx
+++ src/components/pages/loginPage.jsx
@@ -1,5 +1,15 @@
-import { Box, Button, Input } from "@chakra-ui/react"
-import React from "react"
+import {
+  Box,
+  Button,
+  Input,
+  Text,
+  Grid,
+  GridItem,
+  InputGroup,
+  InputRightElement,
+  Checkbox
+} from "@chakra-ui/react"
+import React, { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
 import { ToastContainer, toast } from "react-toastify"
@@ -9,28 +19,92 @@ import { setUserName } from "../store/task"
 const LoginPage = () => {
   localStorage.clear()
   let navigate = useNavigate()
-  const dispatch = useDispatch()
-  const userName = useSelector((state) => state.user.userName)
-  const handleSetUserName = (event) => {
-    dispatch(setUserName(event.target.value))
+  const goRegisterPage = () => {
+    navigate("/register")
   }
 
-  const goNext = () => {
-    if (userName) {
-      navigate("/chat")
-    } else {
-      toast.error("GIVE ME YOUR NAME!")
-    }
-  }
+  const [show, setShow] = useState(false)
+  const handleClick = () => setShow(!show)
 
   return (
-    <Box>
-      <Box>give your name, please</Box>
-      <Input onChange={handleSetUserName} />
-      <Button colorScheme='teal' size='lg' onClick={goNext}>
-        go next
-      </Button>
-      <ToastContainer position='top-center' />
+    <Box
+      backgroundImage='/loginBg.jpg'
+      bgSize='cover'
+      w='100vw'
+      h='100vh'
+      overflow='hidden'
+    >
+      <Box
+        position='absolute'
+        top='50%'
+        left='50%'
+        transform='translate(-50%, -126%)'
+        borderRadius='5px'
+        border='1px solid black'
+        w='500px'
+        h='auto'
+        margin='auto'
+        mt={10}
+        overflow='hidden'
+        bgColor='#2b312d'
+        opacity={0.9}
+      >
+        <Box mt={3} mr={3} textAlign='center'>
+          <Text color='#f4f4f4fa' as='b' textAlign='center'>
+            Добро пожаловать в SimpleChat!
+          </Text>
+        </Box>
+
+        <Box ml={3} mt={3}>
+          <Text color='#fafafafa'>Логиньтесь или будьте гостем</Text>
+        </Box>
+
+        <Box m={2} color='#fafafafa'>
+          <Text m={1}>Введите вашу почту:</Text>
+          <Input
+            // onChange={handleChange}
+            placeholder='Enter e-mail'
+            pr='4.5rem'
+          />
+
+          <Text m={1}>Введите пароль:</Text>
+          <InputGroup size='md'>
+            <Input
+              pr='4.5rem'
+              type={show ? "text" : "password"}
+              placeholder='Enter password'
+            />
+            <InputRightElement width='4.5rem'>
+              <Button h='1.75rem' size='sm' onClick={handleClick}>
+                {show ? "Hide" : "Show"}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+
+          <Grid templateColumns='repeat(5, 1fr)' gap={2} m={5} mr={0} ml={0}>
+            <GridItem>
+              <Button w='110px' h='40px' colorScheme='green'>
+                Login
+              </Button>
+            </GridItem>
+            <GridItem>
+              <Button w='110px' h='40px'>
+                be a guest
+              </Button>
+            </GridItem>
+            <GridItem colStart={5}>
+              <Button
+                w='110px'
+                h='40px'
+                colorScheme='whiteAlpha'
+                onClick={goRegisterPage}
+              >
+                Register
+              </Button>
+            </GridItem>
+          </Grid>
+        </Box>
+      </Box>
     </Box>
   )
 }
