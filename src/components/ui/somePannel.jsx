85d4f7469ae3a20d5e649766001e46242db227c8
gfR616 <616gfr@gmail.com>
2024-01-13 02:51:25 +0500

    Update somePannel.jsx

--- src/components/ui/somePannel.jsx
+++ src/components/ui/somePannel.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const SomePannel = () => {
 	return (
-		<Box w="100%" minH="100vh" border="1px solid black" borderRadius={5}>
+		<Box w="100%" minH="96vh" border="1px solid black" borderRadius={5}>
 			<Box border="1px solid black" borderRadius={5}>
 				SomePannel
 			</Box>
