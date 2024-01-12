9c19dcad489f616302332374c25eb509f987ad3a
gfR616 <616gfr@gmail.com>
2024-01-13 02:51:20 +0500

    Update contactsPannel.jsx

--- src/components/ui/contactsPannel/contactsPannel.jsx
+++ src/components/ui/contactsPannel/contactsPannel.jsx
@@ -1,4 +1,4 @@
-import { Box, Text } from '@chakra-ui/react'
+import { Box, Button, Text } from '@chakra-ui/react'
 import React from 'react'
 import { useSelector } from 'react-redux'
 
@@ -6,13 +6,7 @@ const ContactsPannel = () => {
 	const userName = useSelector((state) => state.user.userName)
 
 	return (
-		<Box w="100%" minH="100vh" border="1px solid black" borderRadius={5}>
-			<Box border="1px solid black" borderRadius={5}>
-				<Text as="b" fontSize="3xl" color="#ff00f2">
-					Hello, {userName}
-				</Text>
-			</Box>
-
+		<Box w="100%" minH="96vh" border="1px solid black" borderRadius={5}>
 			<Box>ContactsPannel. В разработке...</Box>
 		</Box>
 	)
