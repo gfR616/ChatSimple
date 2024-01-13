110c294b7ded3e9c26cf2754b92a8dd27c8f7a4f
gfR616 <616gfr@gmail.com>
2024-01-13 09:54:42 +0500

    обернул в юзер провайдер

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import ChatPage from './components/pages/chatPage'
 import LoginPage from './components/pages/loginPage'
 import RegistrationPage from './components/pages/registrationPage'
 import UserSettingsPage from './components/pages/userSettingsPage'
+import { UsersProvider } from './hooks/useUsers'
 import './scrollBar.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
@@ -10,15 +11,17 @@ import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-d
 function App() {
 	return (
 		<ChakraProvider>
-			<Router>
-				<Routes>
-					<Route exact path="/auth" element={<LoginPage />} />
-					<Route path="/register" element={<RegistrationPage />} />
-					<Route exact path="/chat" element={<ChatPage />} />
-					<Route exact path="/userSettings" element={<UserSettingsPage />}></Route>
-					<Route path="/" element={<Navigate to="/auth" />} />
-				</Routes>
-			</Router>
+			<UsersProvider>
+				<Router>
+					<Routes>
+						<Route exact path="/auth" element={<LoginPage />} />
+						<Route path="/register" element={<RegistrationPage />} />
+						<Route exact path="/chat" element={<ChatPage />} />
+						<Route exact path="/userSettings" element={<UserSettingsPage />} />
+						<Route path="/" element={<Navigate to="/auth" />} />
+					</Routes>
+				</Router>
+			</UsersProvider>
 		</ChakraProvider>
 	)
 }
