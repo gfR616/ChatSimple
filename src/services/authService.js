a127c69cc0cbce3361b9123035f979ee6d916052
gfR616 <616gfr@gmail.com>
2024-01-13 07:06:59 +0500

    Доработал аутх

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,35 +1,55 @@
+import { addNewUser } from './userService'
 import {
 	createUserWithEmailAndPassword,
 	getAuth,
+	onAuthStateChanged,
 	signInWithEmailAndPassword,
-	updateProfile,
+	signOut,
 } from 'firebase/auth'
 
 const auth = getAuth()
-
+//логиним
 export const signIn = async (email, password, navigate) => {
 	try {
-		const userCredential = await signInWithEmailAndPassword(auth, email, password)
-		const user = userCredential.user
+		await signInWithEmailAndPassword(auth, email, password)
 		navigate('/chat')
-		return user
 	} catch (error) {
 		const errorCode = error.code
 		const errorMessage = error.message
 		console.log(`Ошибка авторизации: ${(errorCode, errorMessage)}`)
 	}
 }
-
+//регистрируем и добавляем в фаерстор
 export const register = async (email, password, displayName, navigate) => {
 	try {
 		const userCredential = await createUserWithEmailAndPassword(auth, email, password)
 		const user = userCredential.user
-		await updateProfile(user, { displayName: displayName })
+		const uid = user.uid
+		await addNewUser(displayName, email, uid)
 		navigate('/chat')
 		return user
 	} catch (error) {
 		const errorCode = error.code
 		const errorMessage = error.message
 		console.log(`Ошибка регистрации: ${(errorCode, errorMessage)}`)
+		throw error
 	}
 }
+//выходим из логина
+export const userSignOut = () => {
+	signOut(auth)
+		.then(() => {
+			console.log('Пользователь вышел из системы')
+		})
+		.catch((error) => {
+			console.error('Ошибка при выходе из системы: ', error)
+		})
+}
+//слушаем статус юзера
+onAuthStateChanged(auth, (user) => {
+	if (user) {
+		console.log('User is signed in')
+	} else {
+		console.log('User is signed out')
+	}
+})
