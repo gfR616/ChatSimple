61ebbeedf58bcd211769d5a9bae46dde0ab745cc
gfR616 <616gfr@gmail.com>
2024-01-13 10:03:04 +0500

    вывод имени

--- src/components/ui/userPannel.jsx
+++ src/components/ui/userPannel.jsx
@@ -1,3 +1,4 @@
+import { useUsers } from '../../hooks/useUsers'
 import { userSignOut } from '../../services/authService'
 import { Box, Button, Text } from '@chakra-ui/react'
 import React from 'react'
@@ -12,12 +13,14 @@ const UserPannel = () => {
 		userSignOut()
 		navigate('/auth', { replace: true })
 	}
+
+	const { user } = useUsers()
 	return (
 		<Box border="2px solid black" display="flex" justifyContent="space-between" h="4vh">
 			<Box border="1px solid black" h="35px" w="160px" backgroundImage="logo.svg"></Box>
 			<Box border="1px solid black" borderRadius={5} h="35px">
 				<Text as="b" fontSize="20px" color="#000000">
-					Hello, user (в разработке)
+					{user ? `Hello, ${user.displayName}` : ''}
 				</Text>
 				<Button colorScheme="blue" size="xs" m={1} onClick={goToUserSettings}>
 					Настройки
