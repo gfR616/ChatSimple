d0489040312e6578c9e585d44824b82f18a8ad49
gfR616 <616gfr@gmail.com>
2024-01-13 09:55:34 +0500

    выводим данные со стора

--- src/components/pages/userSettingsPage.jsx
+++ src/components/pages/userSettingsPage.jsx
@@ -1,9 +1,15 @@
+import { useUsers } from '../../hooks/useUsers'
 import { Box } from '@chakra-ui/react'
 import React from 'react'
 
 const UserSettingsPage = () => {
-	// handleGetUserDate = getUserData()
-	return <Box>getUserData()</Box>
+	const { user } = useUsers()
+	return (
+		<>
+			<Box>{user ? `Отображаемое имя: ${user.displayName}` : 'Loading...'}</Box>
+			<Box>{user ? `Email: ${user.email}` : 'Loading...'}</Box>
+		</>
+	)
 }
 
 export default UserSettingsPage
