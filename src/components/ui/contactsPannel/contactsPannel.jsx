f8c25ccf89aa9731950197a5036e33964a1d7ce6
gfR616 <616gfr@gmail.com>
2024-01-16 09:25:15 +0500

    выводим все х юзеров

--- src/components/ui/contactsPannel/contactsPannel.jsx
+++ src/components/ui/contactsPannel/contactsPannel.jsx
@@ -1,11 +1,30 @@
-import { Box, Button, Text } from '@chakra-ui/react'
-import React from 'react'
-import { useSelector } from 'react-redux'
+import { getAllUsers } from '../../../services/userService'
+import { Box } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
 
 const ContactsPannel = () => {
+	const [users, setUsers] = useState()
+
+	useEffect(() => {
+		const handleFetchUsers = async () => {
+			const allUsers = await getAllUsers()
+			setUsers(allUsers)
+		}
+		handleFetchUsers()
+	}, [])
+	console.log('Юзеры запрошены:', users)
 	return (
-		<Box w="100%" minH="96vh" border="1px solid black" borderRadius={5}>
-			<Box>ContactsPannel. В разработке...</Box>
+		<Box w="100%" minH="96vh" border="2px solid black" borderRadius={5}>
+			<Box border="1px black solid">ContactsPannel. В разработке...</Box>
+			<Box border="1px black solid">
+				All Users:
+				{users &&
+					users.map((user) => (
+						<Box key={user.uid} border="1px solid black">
+							{user.displayName}
+						</Box>
+					))}
+			</Box>
 		</Box>
 	)
 }
