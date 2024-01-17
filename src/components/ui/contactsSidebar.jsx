f61dddecc0ce379e33c1eb26c3d9fdbfa65ec15d
gfR616 <616gfr@gmail.com>
2024-01-17 09:45:47 +0500

    убрал паннель, перенес все в сайдбар

---
+++ src/components/ui/contactsSidebar.jsx
@@ -0,0 +1,51 @@
+import { getAllUsers } from '../../services/userService'
+import { ArrowLeftIcon, ArrowRightIcon } from '@chakra-ui/icons'
+import { Box, Button, Text } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
+import { TfiComment, TfiFaceSmile } from 'react-icons/tfi'
+import { Menu, MenuItem, Sidebar, SubMenu } from 'react-pro-sidebar'
+
+const ContactsSidebar = () => {
+	const [collapsed, setCollapsed] = useState(true)
+	const [users, setUsers] = useState()
+	useEffect(() => {
+		const handleFetchUsers = async () => {
+			const allUsers = await getAllUsers()
+			setUsers(allUsers)
+		}
+		handleFetchUsers()
+	}, [])
+	console.log('Юзеры запрошены:', users)
+	return (
+		<Box h="100%" display="flex">
+			<Sidebar collapsed={collapsed}>
+				<Menu>
+					<SubMenu label={collapsed ? <TfiFaceSmile size={30} /> : 'Все пользователи'}>
+						{users &&
+							users.map((user) => (
+								<MenuItem key={user.uid} border="1px solid black">
+									{user.displayName}
+								</MenuItem>
+							))}
+					</SubMenu>
+					<SubMenu
+						label={collapsed ? <TfiComment size={30} /> : 'Мои контакты'}
+					></SubMenu>
+				</Menu>
+			</Sidebar>
+			<Box style={{ marginBottom: 16 }}>
+				<Button
+					checked={collapsed}
+					colorScheme="blackAlpha"
+					onClick={() => setCollapsed(!collapsed)}
+					size="md"
+					m={1}
+				>
+					{collapsed ? <ArrowRightIcon /> : <ArrowLeftIcon />}
+				</Button>
+			</Box>
+		</Box>
+	)
+}
+
+export default ContactsSidebar
