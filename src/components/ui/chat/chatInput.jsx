6b06e1803bfe821bbff02ec87a1e6d894243df4e
gfR616 <616gfr@gmail.com>
2024-01-17 09:44:50 +0500

    подогнар размер текстовой зоны

--- src/components/ui/chat/chatInput.jsx
+++ src/components/ui/chat/chatInput.jsx
@@ -1,14 +1,15 @@
 import { Box, Button, Grid, GridItem, Input, Textarea } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const ChatInput = ({ onInputChange, inputState, onSendMessage }) => {
 	return (
-		<Grid templateColumns="repeat(10, 1fr)" gap={1} m="2">
-			<GridItem colSpan={8}>
+		<Grid templateColumns="repeat(10, 1fr)" gap={1} m={1}>
+			<GridItem colSpan={9}>
 				<Textarea
+					resize="none"
 					borderColor="#946d25"
 					bgColor="#c0bcbc"
-					minH="4vh"
+					minH="6vh"
 					value={inputState}
 					onChange={onInputChange}
 					placeholder="Написать сообщение..."
@@ -19,8 +20,8 @@ const ChatInput = ({ onInputChange, inputState, onSendMessage }) => {
 					}}
 				/>
 			</GridItem>
-			<GridItem colSpan={2}>
-				<Button minH="4vh" w="100%" colorScheme="blue" onClick={onSendMessage}>
+			<GridItem colSpan={1}>
+				<Button minH="6vh" w="100%" colorScheme="blue" onClick={onSendMessage}>
 					Send
 				</Button>
 			</GridItem>
