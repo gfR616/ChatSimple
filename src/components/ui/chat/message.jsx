4704ec1ea4440f026fbc8a5b3102be1167fbd0b3
gfR616 <616gfr@gmail.com>
2024-01-17 09:45:23 +0500

    убрал стрелки

--- src/components/ui/chat/message.jsx
+++ src/components/ui/chat/message.jsx
@@ -12,16 +12,18 @@ const Message = ({ message, user, time, isIncomingMessage }) => {
 				bg="white"
 				minH="fit-content"
 				maxW="45%"
+				minW="10%"
 				borderRadius={10}
 				position="relative"
 				isTruncated={false}
+				border="1px black solid"
 			>
 				<Text
 					as="b"
 					style={{ fontStyle: 'italic' }}
 					color={isIncomingMessage ? '#840903eb' : '#3705e9eb'}
 				>
-					{isIncomingMessage ? '→' : '←'} {user}
+					{user}
 				</Text>
 				<Text isTruncated={false}>{message}</Text>
 				<Text fontSize="xs" color="#2d4812f3" position="absolute" bottom={1} right={5}>
