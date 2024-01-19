e0987fd17208572b691fc4043558e8dcb22913ef
gfR616 <616gfr@gmail.com>
2024-01-19 18:34:01 +0500

    новый сервис

--- src/services/roomService.js
+++ src/services/roomService.js
@@ -1,11 +1,61 @@
-import { getDatabase, ref } from 'firebase/database'
+import { doc, getDatabase, onValue, push, ref, set } from 'firebase/database'
 import { customAlphabet } from 'nanoid'
 
-const db = getDatabase()
-const chatRoomsRef = db.ref('chatRooms')
-const nanoid = customAlphabet('1234567890abcdef', 12)
+//инициализируем базу
+export const getMessagesBase = () => {
+	const db = getDatabase()
+	if (db) {
+		const messagesRef = ref(db, 'base/rooms')
+		return messagesRef
+	} else {
+		console.log('Ошибка при инициализации базы')
+	}
+}
 
-const newChatRoomId = nanoid()
-chatRoomsRef.child(newChatRoomId).set({
-	messages: [],
-})
+//создаем рум
+export const createNewRoom = async (messages) => {
+	const db = getDatabase()
+	const chatRoomsRef = ref(db, 'base/rooms')
+	const nanoid = customAlphabet('1234567890abcdef', 15)
+	const newChatRoomId = nanoid()
+	const newRoomRef = push(chatRoomsRef)
+	await set(newRoomRef, { messages })
+	console.log('Создан новый рум!', newChatRoomId)
+	return newChatRoomId
+}
+//добавляем в рум меседжи
+export const addMessageToRoom = (roomId, message) => {
+	const db = getDatabase()
+	const messagesRef = db.ref(`chatRooms/${roomId}/messages`)
+	messagesRef.push(message)
+	console.log('Сообщения добавлены в рум!', roomId, message)
+}
+//забираем все из рума
+export const getMessagesFromRoom = (roomId) => {
+	return new Promise((resolve, reject) => {
+		const db = getDatabase()
+		const messagesRef = db.ref(`chatRooms/${roomId}/messages`)
+		messagesRef.once(
+			'value',
+			(snapshot) => {
+				resolve(snapshot.val())
+				console.log('Сообщения получены из рума!', roomId)
+			},
+			reject,
+		)
+	})
+}
+//чистим рум
+export const clearRoom = (roomId) => {
+	const db = getDatabase()
+	const messagesRef = db.ref(`chatRooms/${roomId}/messages`)
+	messagesRef.remove()
+	console.log('Сообщения удалены из рума!', roomId)
+}
+//удаляем рум
+export const deleteRoom = (roomId) => {
+	const db = getDatabase()
+	const roomRef = db.ref(`chatRooms/${roomId}`)
+	roomRef.remove()
+	console.log('Рум удален!', roomId)
+}
