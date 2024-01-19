1fb57b7de90749302755c3279a60cd8d6e253428
gfR616 <616gfr@gmail.com>
2024-01-19 12:09:09 +0500

    добавил методы для ключей

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,10 +1,12 @@
 import {
+	arrayUnion,
 	collection,
 	doc,
 	getDoc,
 	getDocs,
 	getFirestore,
 	setDoc,
+	updateDoc,
 } from 'firebase/firestore'
 
 const firestore = getFirestore()
@@ -17,6 +19,7 @@ export const addNewUser = async (displayName, email, uid) => {
 		displayName: displayName,
 		email: email,
 		uid: uid,
+		chats: [],
 	})
 		.then((uid) => {
 			console.log('Document written with ID: ', uid)
@@ -46,5 +49,28 @@ export const getAllUsers = async () => {
 	} catch (error) {
 		console.error('Error fetching users: ', error)
 		throw error
+	}
+}
+// добавляем уникальный ключ к переписке в пользователей
+export const addKeysToUsers = async (senderUid, recipientUid, key) => {
+	const senderDoc = doc(firestore, 'users', senderUid)
+	const recipientDoc = doc(firestore, 'users', recipientUid)
+	await updateDoc(senderDoc, {
+		chats: arrayUnion(key),
+	})
+	await updateDoc(recipientDoc, {
+		chats: arrayUnion(key),
+	})
+}
+//получаем ключи
+export const getKeys = async (senderUid) => {
+	const userDoc = doc(firestore, 'users', senderUid)
+	const userSnapshot = await getDoc(userDoc)
+	if (userSnapshot.exists()) {
+		const userData = userSnapshot.data()
+		return userData.chats
+	} else {
+		console.log('Не удалось получить ключи!')
+		return []
 	}
 }
