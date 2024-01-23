c708a73556c03ca082e546e438751bdef0dc1e4c
gfR616 <616gfr@gmail.com>
2024-01-23 13:37:00 +0500

    залил фейков в базу
    запилил утилиту для добавления фейков

--- src/utils/fakeNames.js
+++ src/utils/fakeNames.js
@@ -1,54 +1,58 @@
-const fakeNames = [
-	{ id: 1, name: 'Синяк' },
-	{ id: 2, name: 'Красавчик' },
-	{ id: 3, name: 'Котик' },
-	{ id: 4, name: 'Шрек' },
-	{ id: 5, name: 'Тапок' },
-	{ id: 6, name: 'Медвежонок' },
-	{ id: 7, name: 'Чебурашка' },
-	{ id: 8, name: 'Крокодил Гена' },
-	{ id: 9, name: 'Волк' },
-	{ id: 10, name: 'Заяц' },
-	{ id: 11, name: 'Пингвин' },
-	{ id: 12, name: 'Тарзан' },
-	{ id: 13, name: 'Слон' },
-	{ id: 14, name: 'Лев' },
-	{ id: 15, name: 'Тигрь' },
-	{ id: 16, name: 'Змей' },
-	{ id: 17, name: 'Кролик' },
-	{ id: 18, name: 'Собака' },
-	{ id: 19, name: 'Кот' },
-	{ id: 20, name: 'Птица' },
-	{ id: 21, name: 'Ловкач' },
-	{ id: 22, name: 'Муха' },
-	{ id: 23, name: 'Пчела' },
-	{ id: 24, name: 'Силач' },
-	{ id: 25, name: 'Жирафик' },
-	{ id: 26, name: 'Енот' },
-	{ id: 27, name: 'Воробей' },
-	{ id: 28, name: 'Сова' },
-	{ id: 29, name: 'Гатс' },
-	{ id: 30, name: 'Гриффитс' },
-	{ id: 31, name: 'Котэ' },
-	{ id: 32, name: 'Клетчатка' },
-	{ id: 33, name: 'Геннадий' },
-	{ id: 34, name: 'Рукаблуд' },
-	{ id: 35, name: 'Ногаблуд' },
-	{ id: 36, name: 'Босс' },
-	{ id: 37, name: 'Товарищь' },
-	{ id: 38, name: 'Горшок' },
-	{ id: 39, name: 'Некий' },
-	{ id: 40, name: 'Клюв' },
-	{ id: 41, name: 'Сайтама' },
-	{ id: 42, name: 'Соник' },
-	{ id: 43, name: 'Веджитта' },
-	{ id: 44, name: 'Наруто' },
-	{ id: 45, name: 'Саске' },
-	{ id: 46, name: 'Кенши Химура' },
-	{ id: 47, name: 'Какаши' },
-	{ id: 48, name: 'Гинтоки' },
-	{ id: 49, name: 'Мадао' },
-	{ id: 50, name: 'Билли Херрингтон' },
+import { collection, doc, getFirestore, setDoc } from 'firebase/firestore'
+import { nanoid } from 'nanoid'
+
+const firestore = getFirestore()
+const usersCollection = collection(firestore, 'users')
+let uid = nanoid()
+const fakedisplayNames = [
+	{
+		chats: [],
+		displayName: 'Синяк',
+		email: '',
+		uid: uid,
+	},
+	{
+		chats: [],
+		displayName: 'Красавчик',
+		email: '',
+		uid: uid,
+	},
+	{
+		chats: [],
+		displayName: 'Котик',
+		email: '',
+		uid: uid,
+	},
+	{
+		chats: [],
+		displayName: 'Шрек',
+		email: '',
+		uid: uid,
+	},
 ]
 
-export default fakeNames
+export const getFakeUsers = () => {
+	fakedisplayNames.forEach((user) => {
+		let uid = nanoid()
+		const userRef = doc(usersCollection, uid)
+		setDoc(userRef, {
+			chats: user.chats,
+			displayName: user.displayName,
+			email: uid,
+			uid: uid,
+		})
+	})
+}
+// chats
+// (array)
+
+// displayName
+// "Мухоморная фея"
+// (string)
+
+// email
+// "realing29@gmail.com"
+// (string)
+
+// uid
+// "vfdgqnA8JLTrxIJUt06jT4Vo6Hw1"
