6e9130dd33a130ea5d4008900f6e5d525e26d7b8
gfR616 <616gfr@gmail.com>
2024-01-25 13:25:39 +0500

    добавил все переписки, применил мап

--- src/components/ui/chatsBar/chatsList/chatsList.jsx
+++ src/components/ui/chatsBar/chatsList/chatsList.jsx
@@ -1,21 +1,29 @@
+import { UseChats } from '../../../../hooks/useChats'
 import ChatListElement from './chatListElement'
 import { Box } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const ChatsList = () => {
+export const ChatsList = ({ senderUid }) => {
+	const { getAllUserChats } = UseChats()
+	const [userChatList, setUserChatList] = useState([])
+
+	useEffect(() => {
+		const fetchUserChats = async () => {
+			if (senderUid) {
+				const chats = await getAllUserChats(senderUid)
+				setUserChatList(chats)
+			}
+		}
+		fetchUserChats()
+	}, [senderUid, getAllUserChats])
+
+	userChatList && console.log('ahahahah', userChatList)
 	return (
-		<Box border="1px black solid" h="87vh" borderRadius={5} overflow="auto">
-			<ChatListElement />
-			<ChatListElement />
-			<ChatListElement />
-			<ChatListElement />
-			<ChatListElement />
-			<ChatListElement />
-			<ChatListElement />
-			<ChatListElement />
-			<ChatListElement />
-			<ChatListElement />
-			<ChatListElement />
+		<Box border="1px black solid" h="86.8vh" borderRadius={5} overflow="auto">
+			{userChatList &&
+				userChatList.map((chat, index) => {
+					return <ChatListElement key={index} message={chat.message} time={chat.time} />
+				})}
 		</Box>
 	)
 }
