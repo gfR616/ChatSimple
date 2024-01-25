a3072e607cc91f45017e5a97b083dbc2a0a2e1e9
gfR616 <616gfr@gmail.com>
2024-01-25 13:25:11 +0500

    что-то вывел, нужен реципиент

--- src/components/ui/chatsBar/chatsList/chatListElement.jsx
+++ src/components/ui/chatsBar/chatsList/chatListElement.jsx
@@ -1,10 +1,14 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Text } from '@chakra-ui/react'
 import React from 'react'
 
-const ChatListElement = () => {
+const ChatListElement = ({ message, time }) => {
 	return (
-		<Box h="10vh" border="1px red solid" borderRadius={5}>
-			Чат с кем-то
+		<Box h="10vh" border="1px black solid" borderRadius={5} bgColor="blue.100">
+			<Text as="b" style={{ fontStyle: 'italic' }} color={'#840903eb'}></Text>
+			<Text isTruncated={false}>{message}</Text>
+			<Text fontSize="xs" color="#2d4812f3">
+				{time}
+			</Text>
 		</Box>
 	)
 }
