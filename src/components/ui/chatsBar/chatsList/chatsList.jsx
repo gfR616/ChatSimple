b14d3687c01b0a6881c0847b43b963f1acee1a9d
gfR616 <616gfr@gmail.com>
2024-01-26 06:16:44 +0500

    Update chatsList.jsx

--- src/components/ui/chatsBar/chatsList/chatsList.jsx
+++ src/components/ui/chatsBar/chatsList/chatsList.jsx
@@ -6,6 +6,7 @@ import React, { useEffect, useState } from 'react'
 export const ChatsList = ({ senderUid }) => {
 	const { getAllUserChats } = UseChats()
 	const [userChatList, setUserChatList] = useState([])
+	const [lastMessage, setLastMessage] = useState([])
 
 	useEffect(() => {
 		const fetchUserChats = async () => {
@@ -16,13 +17,13 @@ export const ChatsList = ({ senderUid }) => {
 		}
 		fetchUserChats()
 	}, [senderUid, getAllUserChats])
-
-	userChatList && console.log('ahahahah', userChatList)
+	userChatList && console.log('userChatList', userChatList)
 	return (
 		<Box border="1px black solid" h="86.8vh" borderRadius={5} overflow="auto">
 			{userChatList &&
-				userChatList.map((chat, index) => {
-					return <ChatListElement key={index} message={chat.message} time={chat.time} />
+				userChatList.map((chatItem, index) => {
+					console.log('ЧАТ', chatItem)
+					return <ChatListElement key={index} latestMessage={chatItem.latestMessage} />
 				})}
 		</Box>
 	)
