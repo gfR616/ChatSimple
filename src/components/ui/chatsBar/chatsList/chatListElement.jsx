ca3f908cbf6db15fcd1544d419561ada2a1c672d
gfR616 <616gfr@gmail.com>
2024-01-26 06:16:36 +0500

    выводим чат

--- src/components/ui/chatsBar/chatsList/chatListElement.jsx
+++ src/components/ui/chatsBar/chatsList/chatListElement.jsx
@@ -1,13 +1,36 @@
+import { useInitialChat } from '../../../../hooks/useInitialChat'
+import { setRecipientUid } from '../../../../store/task'
 import { Box, Text } from '@chakra-ui/react'
 import React from 'react'
+import { useDispatch } from 'react-redux'
 
-const ChatListElement = ({ message, time }) => {
+const ChatListElement = ({ latestMessage }) => {
+	if (!latestMessage) {
+		return null
+	}
+	const { InitialChat } = useInitialChat()
+	const dispatch = useDispatch()
+	const handleOpenChat = async (senderUid, recipientUid) => {
+		InitialChat(senderUid, recipientUid)
+		dispatch(setRecipientUid(recipientUid))
+	}
+
 	return (
-		<Box h="10vh" border="1px black solid" borderRadius={5} bgColor="blue.100">
-			<Text as="b" style={{ fontStyle: 'italic' }} color={'#840903eb'}></Text>
-			<Text isTruncated={false}>{message}</Text>
+		<Box
+			h="10vh"
+			border="1px black solid"
+			borderRadius={5}
+			bgColor="blue.100"
+			onClick={() =>
+				handleOpenChat(latestMessage.senderUid ?? '', latestMessage.recipientUid ?? '')
+			}
+		>
+			<Text as="b" style={{ fontStyle: 'italic' }} color={'#172c69eb'}>
+				{latestMessage.recipientName}
+			</Text>
+			<Text isTruncated={true}>{latestMessage.message}</Text>
 			<Text fontSize="xs" color="#2d4812f3">
-				{time}
+				{latestMessage.displayDate}
 			</Text>
 		</Box>
 	)
