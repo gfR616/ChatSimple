af2e5c95f3100b0c7d4a16e9b8ec260d3db61549
gfR616 <616gfr@gmail.com>
2024-01-27 08:06:22 +0500

    перенес функционал

--- src/components/ui/chatsBar/chatsBar.jsx
+++ src/components/ui/chatsBar/chatsBar.jsx
@@ -1,41 +1,29 @@
 import { UseChats } from '../../../hooks/useChats'
 import { useInitialChat } from '../../../hooks/useInitialChat'
-import { getAllUsers } from '../../../services/userService'
 import { setRecipientUid } from '../../../store/task'
 import ChatsList from './chatsList'
 import { ArrowLeftIcon, ArrowRightIcon } from '@chakra-ui/icons'
-import { Box, Button, Menu, Text } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
-// import { TfiComment, TfiFaceSmile, TfiIdBadge } from 'react-icons/tfi'
 import { useDispatch } from 'react-redux'
 
 const ChatsBar = ({ senderUid }) => {
-	const dispatch = useDispatch()
 	const { getAllUserChats } = UseChats()
-	const { InitialChat } = useInitialChat()
 	const [userChatList, setUserChatList] = useState()
-	const [isEmptyInput, setisEmptyInput] = useState()
-	console.log('усер чат лист', userChatList)
 
 	useEffect(() => {
 		const fetchUserChats = async () => {
 			if (senderUid) {
 				const chats = await getAllUserChats(senderUid)
 				setUserChatList(chats)
-				setisEmptyInput(chats)
 			}
 		}
 		fetchUserChats()
 	}, [senderUid, getAllUserChats])
 
-	const handleOpenChat = async (recipientUid) => {
-		console.log(senderUid, recipientUid)
-		dispatch(setRecipientUid(recipientUid))
-		InitialChat(senderUid, recipientUid)
-	}
 	return (
 		<Box h="95vh" bgColor="gray.100" border="1px black solid" borderRadius={5}>
-			<ChatsList userChatList={userChatList} />
+			<ChatsList userChatList={userChatList} senderUid={senderUid} />
 		</Box>
 	)
 }
