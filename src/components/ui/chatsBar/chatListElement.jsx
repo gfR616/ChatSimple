005998e747b19180b22cef270920b1c3a0f39680
gfR616 <616gfr@gmail.com>
2024-01-27 13:38:08 +0500

    отображение имени чата+диспатч

--- src/components/ui/chatsBar/chatListElement.jsx
+++ src/components/ui/chatsBar/chatListElement.jsx
@@ -1,18 +1,20 @@
 import { useInitialChat } from '../../../hooks/useInitialChat'
-import { setRecipientUid } from '../../../store/task'
+import { setDisplayChatName, setRecipientUid } from '../../../store/task'
 import { Box, Text } from '@chakra-ui/react'
 import React from 'react'
 import { useDispatch } from 'react-redux'
 
-const ChatListElement = ({ latestMessage }) => {
+const ChatListElement = ({ latestMessage, chatDisplayName }) => {
 	if (!latestMessage) {
 		return null
 	}
 	const { InitialChat } = useInitialChat()
 	const dispatch = useDispatch()
+
 	const handleOpenChat = async (senderUid, recipientUid) => {
 		InitialChat(senderUid, recipientUid)
 		dispatch(setRecipientUid(recipientUid))
+		dispatch(setDisplayChatName('New chat name'))
 	}
 
 	return (
@@ -26,11 +28,13 @@ const ChatListElement = ({ latestMessage }) => {
 			}
 		>
 			<Text as="b" style={{ fontStyle: 'italic' }} color={'#172c69eb'}>
-				{latestMessage.recipientName}
+				{chatDisplayName}
 			</Text>
 			<Text isTruncated={true}>{latestMessage.message}</Text>
 			<Text fontSize="xs" color="#2d4812f3">
 				{latestMessage.displayDate}
+				<span> </span>
+				{latestMessage.displayTime}
 			</Text>
 		</Box>
 	)
