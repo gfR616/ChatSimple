6bcce3511f533dbfc261d0e51444519224e6695a
gfR616 <616gfr@gmail.com>
2024-01-27 13:38:31 +0500

    +проверка

--- src/components/ui/chatsBar/chatsBar.jsx
+++ src/components/ui/chatsBar/chatsBar.jsx
@@ -1,11 +1,8 @@
 import { UseChats } from '../../../hooks/useChats'
-import { useInitialChat } from '../../../hooks/useInitialChat'
-import { setRecipientUid } from '../../../store/task'
 import ChatsList from './chatsList'
 import { ArrowLeftIcon, ArrowRightIcon } from '@chakra-ui/icons'
 import { Box } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux'
 
 const ChatsBar = ({ senderUid }) => {
 	const { getAllUserChats } = UseChats()
@@ -23,7 +20,7 @@ const ChatsBar = ({ senderUid }) => {
 
 	return (
 		<Box h="95vh" bgColor="gray.100" border="1px black solid" borderRadius={5}>
-			<ChatsList userChatList={userChatList} senderUid={senderUid} />
+			{userChatList && <ChatsList userChatList={userChatList} senderUid={senderUid} />}
 		</Box>
 	)
 }
