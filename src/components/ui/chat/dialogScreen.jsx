6a16ac70af7b4252216ffbd45a85a32605907493
gfR616 <616gfr@gmail.com>
2024-01-27 14:12:49 +0500

    фильтруем сообщения по направленности

--- src/components/ui/chat/dialogScreen.jsx
+++ src/components/ui/chat/dialogScreen.jsx
@@ -1,3 +1,4 @@
+import { useUsers } from '../../../hooks/useUsers'
 import Message from './message'
 import { Box } from '@chakra-ui/react'
 import React, { useEffect, useRef } from 'react'
@@ -5,12 +6,21 @@ import { useSelector } from 'react-redux'
 
 const DialogScreen = () => {
 	const boxRef = useRef()
+	const { user } = useUsers()
 	const displayState = useSelector((state) => state.all.displayState)
 	useEffect(() => {
 		if (boxRef.current) {
 			boxRef.current.scrollTop = boxRef.current.scrollHeight
 		}
 	}, [displayState])
+
+	const filteredDisplayState = displayState.map((messageObj) => {
+		if (messageObj.senderName === user.displayName) {
+			return { ...messageObj, isIncoming: false }
+		} else {
+			return { ...messageObj, isIncoming: true }
+		}
+	})
 
 	return (
 		<Box
@@ -23,8 +33,9 @@ const DialogScreen = () => {
 			overflow="auto"
 			opacity="0.95"
 		>
-			{displayState &&
-				displayState.map((messageObj) => {
+			{filteredDisplayState &&
+				filteredDisplayState.map((messageObj) => {
+					console.log('messageObj', messageObj)
 					return (
 						<Box
 							key={messageObj._id}
